ed3cd3445b29bb10141c26091a5f1fb150e57b86
jinusong <sugwer7464@gmail.com>
2018-10-20 22:55:10 +0900

    [Update] add room

--- app.js
+++ app.js
@@ -1,72 +1,77 @@
-var app = require('express')();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+// socket 서버 열기 : 7000포트
+var io = require('socket.io').listen(7000);
 
+// 단어 배열 선언
 var words = new Array('송진우', '돌맹이', '나뭇가지', '충전기', '노트북')
-var queue = [];
-var room;
 
+// 레디가 된 소켓의 수
+var count = 0;
+
+// 그리는 사람 지정
+var peer;
+
+// 단어 랜덤 선택
 function randomitem(a) {
-  var sibre = a[Math.floor(Math.random()*a.length)];
-  console.log(sibre);
-  return sibre;
+  var ritem = a[Math.floor(Math.random()*a.length)];
+  console.log(ritem);
+  return ritem;
 }
 
+// 소켓과 연결이 됐을 때
 io.on('connection', function(socket) {
   console.log("user connect : " + socket.id);
+  socket.emit('socketConnect', 'socketConnect : '+ socket.id);
 
   socket.on('ready', function() {
-    findPeer(socket);
-  });
+    count++;
+    console.log(count);
+  
+    // 먼저 들어온 사람이 그리는 사람이 됨
+    if(count === 1) {
+        peer = socket;
+    } 
 
-  socket.on('color', function(color, weight){ // App1
-    socket.emit('color', color, weight); // App2
-  });
+    // 2번째로 온사람이 맞추는 사람이 됨과 동시에
+    else if(count === 2){
+        // 랜덤 단어 선택
+        var randomwords = randomitem(words); 
+        
+        // 방 생성 및 입장
+        peer.join('room');
+        socket.join('room');
 
-  socket.on('correct', function() { // App2
-    console.log('Correct');
-    socket.emit('correct'); // App1, App2
-  });
+        // 각각의 소켓에게 0:(그림)과 1:(맞춤)로 전송 
+        peer.emit('all ready', '0');
+        socket.emit('all ready', '1');
 
-  socket.on('EVENT_SENDER', function(x, y, event){ // App1
-    socket.emit('EVENT_Receiver', x, y, event) //App2
-  });
-
-  socket.on('disconnect', function() {
-    console.log('user disconnected : ' + socket.id)
-    count = -1;
-  });
-});
-
-
-var findPeer = function(socket) {
-  console.log("queue.length : "+ queue.length);
-  if (queue.length > 0) {
-      var peer = queue.pop();
-      randomwords = randomitem(words)
-      if(peer == socket) {
-          findPeerForLoneSocket(socket);
+        // 랜덤 단어 전송
+        io.sockets.in('room').emit('onConnect', randomwords);  // room 안의 모든 소켓에 emit
+        io.to(socket.id).emit('onConnect', randomwords);
+        io.to(peer.id).emit('onConnect', randomwords);
       }
-      console.log("success");
+  });
 
-      room = socket.id+'#'+peer.id;
-      peer.join(room);
-      socket.join(room);
+  // 색상과 두께를 받고, 보내는 소켓을 제외한 방안의 모든 소켓에게 전송
+  socket.on('color', function(color, weight){
+    io.sockets.in('room').emit('color', color, weight); // room안의 '본인'을 제외한 모든 소켓에 emit
+  });
 
-      peer.emit('all ready', 0);
-      socket.emit('all ready', 1);
+  // 상대가 맞췄는지 확인
+  socket.on('correct', function() {
+    console.log('Correct');
+    io.sockets.in('room').emit('correct');
+  });
 
-      peer.emit('connect', randomwords);
-      socket.emit('connect', randomwords);
-      console.log("값 전달 완료")
-  } else {
-      console.log('push queue');
-      queue.push(socket);
-      console.log("queue" + queue);
-  }
-}
+  // x, y, 이벤트 전송
+  socket.on('EVENT_SENDER', function(x, y, event){
+    console.log('event sender!');
+    socket.broadcast.to('room').emit('EVENT_Receiver', x, y, event);
+  });
 
-//서버를 시작한다. 
-server.listen(7000, function(){
-  console.log("server on 7000");
+  // 소켓의 연결이 해제 됨
+  socket.on('disconnect', function(socket) {
+    console.log('user disconnected : ' + socket.id);
+    count = 0; // 하나라도 연결을 해제하면 0으로 초기화
+    console.log(count);
+  });
 });
